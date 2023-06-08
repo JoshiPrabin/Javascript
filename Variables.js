0edfd1af544b11f2cb22d09dc770fad2d0d7a72b
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-08 14:33:49 +0545

    Variables in javascript

--- Variables.js
+++ Variables.js
@@ -7,3 +7,6 @@ var myName = "Prabin Joshi";
 // The var keyword was used in all JavaScript code from 1995 to 2015.
 //The let and const keywords were added to JavaScript in 2015.
 let myLastName = "Joshi";
+
+//The biggest problem with 'var' is that you can easily overwrite variable declarations
+//'let' solves this problem. It throws an error whenever you try to overwrite a varaible.
