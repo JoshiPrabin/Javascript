74eb303f46b516b6ebe90ac6be7c8c40ca9aa154
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-08 14:21:22 +0545

    Variables in javascript

---
+++ Variables.js
@@ -0,0 +1,9 @@
+/* Javascript provides eight different data types i.e. null, undefined, string, number, symbol,
+object, bigint and boolean*/
+
+//declaring a variable
+var myName = "Prabin Joshi";
+
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+//The let and const keywords were added to JavaScript in 2015.
+let myLastName = "Joshi";
