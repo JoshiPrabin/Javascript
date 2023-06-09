6e38c64ae1f6dc05b2c645219f176f88b69fb5d0
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-09 20:41:31 +0545

    Boolean data type

---
+++ Boolean.js
@@ -0,0 +1,12 @@
+let isTrue;
+
+function check(isTrue){
+    if(isTrue == true){
+        return "True = Yup";
+    }
+    else{
+        return "True = Nope";
+    }
+}
+
+console.log(check(false));
