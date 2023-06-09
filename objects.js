6c38048048045d2b5ff8c62c935802bffc6fdd9b
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-09 22:38:01 +0545

    Objects in javascript and accessing properties using dot operator

---
+++ objects.js
@@ -0,0 +1,11 @@
+// This is an object
+const object = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  const Hat = object.hat;      
+  const Shirt = object.shirt;  
+  console.log(Hat);
+  console.log(Shirt);
