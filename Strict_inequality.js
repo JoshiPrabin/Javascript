576bb150f5930293e1acc8d7a985fb077af6cc40
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-09 21:04:57 +0545

    Strict inequality operator

---
+++ Strict_inequality.js
@@ -0,0 +1,12 @@
+let val;
+
+function check(val){
+    if(val !== 3){
+        return "Not the same as the number 3";
+    }
+    else{
+        return "Same as the number 3";
+    }
+}
+
+console.log(check('3'));
