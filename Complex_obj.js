5148a191cb8d67a6e7d27002409e407467b8300f
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-10 06:25:03 +0545

    Manipulating complex objects

---
+++ Complex_obj.js
@@ -0,0 +1,26 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  
+  myMusic.push({
+    "artist": "Gregory Alan Isakov",
+    "title": "If i go, I'm goin",
+    "release_year": 1995,
+    "formats": [
+      "CD",
+      "8T",
+      "Lp"
+    ]
+  })
+
+  console.log(myMusic);
