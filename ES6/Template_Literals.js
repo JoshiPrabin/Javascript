49d6524ebdc99f2c30a43e3f12cafd6ad6b568a7
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-12 10:16:13 +0545

    Using template literals

---
+++ ES6/Template_Literals.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "Prabin Joshi",
+    age: 21
+  };
+  
+  const greeting = `Hello, my name is ${person.name}!
+  I am ${person.age} years old.`;
+  
+  console.log(greeting);
