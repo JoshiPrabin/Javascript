9b3dbfcdd07f2a3a9876841676d3bd6891732ae7
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-12 21:58:27 +0545

    Some other shorthand character classes

--- Regular Expressions/Other_shorthand_classes.js
+++ Regular Expressions/Other_shorthand_classes.js
@@ -1,3 +1,7 @@
 // \W is the opposite of \w i.e. everything except alphanumeric characters and _
 
 // \d is for matching all numbers
+
+// \s for whitespaces, carriage return, tab, form feed, and new line characters
+
+// \S is the opposite of \s
