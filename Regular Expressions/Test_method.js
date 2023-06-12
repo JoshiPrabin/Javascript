32349ad3472e56734ff227926649b8b90a3c67d9
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-12 16:52:17 +0545

    Using the test method

---
+++ Regular Expressions/Test_method.js
@@ -0,0 +1,4 @@
+let myString = "Hello, World!";
+let myRegex = /Hello/; // this is the regex
+let result = myRegex.test(myString); //test() method returns true or flase based on if the pattern in myRegex is present in myString or not.
+console.log(result);
