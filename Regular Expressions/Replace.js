5feb79613632ef58563dfa234fba369cb96b3453
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-14 13:42:48 +0545

    Match and replace

--- Regular Expressions/Replace.js
+++ Regular Expressions/Replace.js
@@ -1,5 +1,5 @@
 let str = "This is a random string";
-let regEx = /(\w+)/;
-let result = str.replace(regEx , "Random");
+let regEx = /(\w+)\s(\w+)\s(\w+)\s(\w+)\s(\w+)/;
+let result = str.replace(regEx , "$5 $4 $3 $2 $1");
 
 console.log(result);
