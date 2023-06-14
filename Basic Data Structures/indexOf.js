862c36961dd1f077e411862a3e1ea1ca5ef486e5
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-14 17:32:38 +0545

    Using indexOf to check if an element exists in an array

---
+++ Basic Data Structures/indexOf.js
@@ -0,0 +1,10 @@
+function quickCheck(arr, elem) {
+    if(arr.indexOf(elem)== -1){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
