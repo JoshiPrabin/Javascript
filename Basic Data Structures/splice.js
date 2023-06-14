02c5d7fb6b1b674fc4517a0dfa37ece87c6e2265
Prabin Joshi <prabinjoshi68@gmail.com>
2023-06-14 17:09:15 +0545

    The splice method

--- Basic Data Structures/splice.js
+++ Basic Data Structures/splice.js
@@ -2,5 +2,5 @@
 
 const arr = [2, 4, 5, 1, 7, 5, 2, 1];
 arr.shift();
-arr.splice(2,4);
+arr.splice(2,4); // first argument is the index from where to start removing and second is for how many items to remove
 console.log(arr);
